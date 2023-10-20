b27853ac55a196e97d533c8facbcd5fd05fa9f5e
YannamYeswanth <122900312+YannamYeswanth@users.noreply.github.com>
2023-10-20 23:05:06 +0530

    updates

--- Parcel_delivery/courier_service/models.py
+++ Parcel_delivery/courier_service/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    UserId=models.IntegerField()
+    name=models.CharField(max_length=50)
+    Contact_Number=models.BigIntegerField()
+    Email=models.EmailField()
+    House_No=models.CharField(max_length=50)
+    Street=models.CharField(max_length=50)
+    City=models.CharField(max_length=50)
+    State=models.CharField(max_length=50)
+    Pin_Code=models.IntegerField()
+    
