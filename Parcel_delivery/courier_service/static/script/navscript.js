41cb15546ece1c5e57c77bc6c9f005bcef7f1f61
Vedant4581 <vedant4947@gmail.com>
2023-10-31 11:48:10 +0530

    changes

---
+++ Parcel_delivery/courier_service/static/script/navscript.js
@@ -0,0 +1,8 @@
+const hidenavbar=()=>{
+    const sidebar=document.querySelector('.side_bar')
+    sidebar.style.display='none'
+  }
+  
+  const shownavbar=()=>{
+    document.querySelector('.side_bar').style.display='block'
+  }
