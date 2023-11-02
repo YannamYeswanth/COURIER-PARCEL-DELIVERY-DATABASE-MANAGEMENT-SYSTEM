de2a43a484d66b0868b8a67a3bea0f87dcb3337d
Sriyans-Reddy <vsriyansreddy2005@gmail.com>
2023-11-02 23:01:32 +0530

    price update

--- Parcel_delivery/courier_service/views.py
+++ Parcel_delivery/courier_service/views.py
@@ -232,7 +232,7 @@ def estimate(request):
         dist=geopy.distance.geodesic(co1,co2).km
         weight=int(weight)
         value=int(tod)
-        cost=int(((dist*1+500)+(vol*0.0001))*weight*value)
+        cost=int(((dist*0.4+50)+(vol*0.0001))*weight*value/2)
         time=int((dist*0.01)/value)+1
         context['cost']=cost
         context['time']=time
